c6f8c1f8b080efcaa79299802fca5a36c1d17e1f
Pelumiii1 <adesokanpelumi14@gmail.com>
2022-07-12 10:39:58 +0100

    update

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,10 +3,10 @@
 /**
  * reset_to_98 -> a function to update the refence of a pointer
  * @n: pointer to n to be updated
- * 
- * 
+ *
+ *
  */
 void reset_to_98(int *n)
 {
-    *n = 98;
+*n = 98;
 }
