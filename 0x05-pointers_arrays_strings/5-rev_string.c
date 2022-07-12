fb1509c8c315c7a4c5b54d2089f61facbdfe1c1c
Pelumiii1 <adesokanpelumi14@gmail.com>
2022-07-12 12:23:03 +0100

    update

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -15,13 +15,12 @@ int i, length1, length2;
  while (s[length1] != '\0')
  length1++;
 
- lenght2 = length1 - 1;
+ length2 = length1 - 1;
  for (i = 0; i < length1 / 2; i++)
  {
     tmp = s[i];
     s[i] = s[length2];
     s[length2] = tmp;
     length2 -= 1;
- }
  }
 }
