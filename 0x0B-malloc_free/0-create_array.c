b90e781ea1b2f769859738a115cfc587485bdb68
Pelumiii1 <adesokanpelumi14@gmail.com>
2022-07-25 16:37:35 +0100

    update

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -8,13 +8,13 @@
  */
 char *create_array(unsigned int size, char c)
 {
-	char *p;
+	char p;
 	unsigned int i;
 
 	if (size == 0)
 	return (0);
-	*p = malloc((size) * sizeof(char));
-	if (*p == '\0')
+	p = malloc((size) * sizeof(char));
+	if (p == '\0')
 	return (0);
 	for (i = 0; i < size; i++)
 	{
