960e2afb764e95a3def1563436cf393e1731c5dd
Pelumiii1 <adesokanpelumi14@gmail.com>
2022-08-02 16:45:53 +0100

    update

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - check the code for Holberton School students.
+ * 
+ * Return: Always 0
+ */
+
+int msin(void)
+{
+    printf("%s\n", __FILE__);
+    return (0);
+}
