0cfcfcf522b7d6d5189a7e70ca1237436d5021db
Pelumiii1 <adesokanpelumi14@gmail.com>
2022-08-18 17:49:36 +0100

    update

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -5,12 +5,13 @@
  * @n: parameter
  * @index: index
  * Return: 1 if works, -1 if error
- * 
+ *
  */
 
-int clear_bit(unsigned long int *n, unsigned int index){
-if(index > sizeof(n) * 8)
-return (-1);
-*n &= ~(1 << index);
-return (1);
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+    if (index > sizeof(n) * 8)
+        return (-1);
+    *n &= ~(1 << index);
+    return (1);
 }
