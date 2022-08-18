434db27a39507d9252fa6e24fcef7174970cbefe
Pelumiii1 <adesokanpelumi14@gmail.com>
2022-08-18 17:48:22 +0100

    update

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *get_bit - gets the value of a bit at a given index
+ @n : the decimal parameter
+ * @index: index
+ * Return: the value
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+    int bit;
+
+    bit = (n >> index);
+    if (index > 32)
+        return (-1);
+    return (bit & 1);
+}
